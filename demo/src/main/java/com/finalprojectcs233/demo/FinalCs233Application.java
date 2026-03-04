d7774f6758f9bc7453ca821aa0e3f11b92520de0
“vsak881”~ <Seng.Vireaksak@gmail.com>
2026-03-04 21:36:20 +0700

    initial commit: spring boot project setup

---
+++ demo/src/main/java/com/finalprojectcs233/demo/FinalCs233Application.java
@@ -0,0 +1,13 @@
+package com.finalprojectcs233.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalCs233Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalCs233Application.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/finalprojectcs233/demo/FinalCs233ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalprojectcs233.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalCs233ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
